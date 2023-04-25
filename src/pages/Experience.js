8b37888da0e860b726b54aff20b4b5371b47cd14
ewa1do <this.eduardovera@gmail.com>
2023-04-25 11:07:39 -0400

    adding responsive design to the work experience section

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -25,7 +25,7 @@ const workExp = [
         duration: 'Jan 2023 - Mar 2023',
         description: [
             'Freelance project to rebuild the business landing page.',
-            'Work with react.js and framer motion.',
+            'Work with React.js and Framer motion.',
         ],
         link: 'https://www.countop.com',
     },
@@ -64,25 +64,30 @@ function Experience() {
     }
 
     return (
-        <section className="pl-10 pb-40 -mt-20">
-            <Title>Work Experience</Title>
-            <div>
-                <div className="flex pb-6" ref={buttonRef}>
+        <section className="pl-10 pb-40 -mt-20 md:-mt-60">
+            <div className="md:ml-[25%] mb-8">
+                <Title>Work Experience</Title>
+            </div>
+            <div className="md:flex md:justify-center">
+                <div
+                    className="flex md:flex-col md:items-start pb-6"
+                    ref={buttonRef}
+                >
                     {companies.map((comp, i) => (
                         <button
                             key={i}
                             className={`${
                                 i === 0 ? 'text-neon-blue' : 'text-slate-300'
-                            } pr-9 text-xl`}
+                            } pr-9 text-xl md:py-2 md:text-2xl lg:text-3xl`}
                             onClick={currentWorkHandler}
                         >
                             {comp}
                         </button>
                     ))}
                 </div>
-                <div>
+                <div className="md:w-3/5">
                     <h3
-                        className={`${roboto.className} text-slate-300 text-3xl`}
+                        className={`${roboto.className} text-slate-300 text-3xl lg:text-4xl`}
                     >
                         {role}{' '}
                         <span className="text-neon-blue">
@@ -97,15 +102,32 @@ function Experience() {
                         </span>
                     </h3>
                     <p
-                        className={`${spaceMono.className} text-dark-gray text-xl`}
+                        className={`${spaceMono.className} text-dark-gray text-xl lg:text-2xl`}
                     >
                         {duration}
                     </p>
-                    <ul className={`${spaceMono.className}`}>
+                    <ul
+                        className={`${spaceMono.className} flex flex-col justify-center`}
+                    >
                         {description.map((desc, i) => (
-                            <li key={`desc-${i + i}`} className="flex py-4 ">
-                                <Marker />
-                                <p className="text-dark-gray px-2 text-xl">
+                            <li
+                                key={`desc-${i + i}`}
+                                className="flex items-center py-4 "
+                            >
+                                <svg
+                                    width="20"
+                                    height="10"
+                                    viewBox="0 0 15 10"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className=""
+                                >
+                                    <path
+                                        d="M14.4731 5.05639L3.67164 9.25303L3.75456 0.690513L14.4731 5.05639Z"
+                                        fill="#00FFD1"
+                                    />
+                                </svg>
+                                <p className="text-dark-gray ml-2 text-xl md:text-2xl">
                                     {desc}
                                 </p>
                             </li>
