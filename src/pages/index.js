47e0d59d6149c1cefe5957e899dac313785ec718
ewa1do <this.eduardovera@gmail.com>
2023-05-03 18:09:26 -0400

    refactoring pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,11 @@ import { Space_Mono } from 'next/font/google'
 
 import NavBar from '@/components/nav'
 
-import About from './About'
-import Contact from './Contact'
-import Experience from './Experience'
-import HomePage from './HomePage'
-import Projects from './Projects'
+import About from '@/components/About'
+import Contact from '@/components/Contact'
+import Experience from '@/components/Experience'
+import HomePage from '@/components/HomePage'
+import Projects from '@/components/Projects'
 
 import useScreenSize from '@/hooks/useScreenSize'
 import useObserver from '@/hooks/useObserver'
