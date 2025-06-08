aae2491611660346d6b4febb8e7dda36cde92c62
Eduardo Vera <this.eduardovera@gmail.com>
2025-06-08 01:12:10 -0400

    refactor homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,27 +1,28 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from "react";
 
-import NavBar from '@/components/nav'
-import ButtonDownload from '@/components/buttons/ButtonDownload'
-import getFonts from '@/utils/fonts'
+import NavBar from "@/components/nav";
+import ButtonDownload from "@/components/buttons/ButtonDownload";
+import getFonts from "@/utils/fonts";
 
-const { roboto, spaceMono } = getFonts()
+const { roboto, spaceMono } = getFonts();
 
 function HomePage(props) {
-    const sectRef = useRef()
+    const sectRef = useRef();
 
-    useEffect(() => {
-        setTimeout(() => {
-            sectRef.current.classList.remove('section--fade')
-        }, 3000)
-    }, [])
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         sectRef.current.classList.remove('section--fade')
+    //     }, 3000)
+    // }, [])
 
     return (
-        <section className="md:pb-48 lg:pb-28">
+        <section>
             <NavBar />
 
             <div
                 ref={sectRef}
-                className={`${spaceMono.className} section section--fade flex flex-col justify-center px-4 mt-20 md:mt-60 ml-[6%]`}
+                // className={`${spaceMono.className} section section--fade flex flex-col justify-center px-4 mt-20 md:mt-60 ml-[6%]`}
+                className={`${spaceMono.className} section flex flex-col justify-center items-center px-4 mt-32 mb-48`}
             >
                 <h5
                     className={`text-neon-blue text-xl sm:text-2xl md:text-3xl lg:text-4xl md:mb-5`}
@@ -37,12 +38,11 @@ function HomePage(props) {
                 <p
                     className={`${roboto.className} text-dark-gray py-3 mb-4 text-2xl md:text-3xl w-full sm:w-4/5 md:w-4/5 xl:w-3/5 xl:text-4xl`}
                 >
-                    I&apos;m a software developer with expertise in both{' '}
+                    I&apos;m a software developer with expertise in both{" "}
                     <span className="text-neon-blue">backend </span>
-                    and <span className="text-neon-blue">frontend</span>{' '}
-                    development. I also have a knack for{' '}
-                    <span className="text-neon-blue">design</span> and enjoy
-                    building cool stuff. Thank you for visiting my portfolio!{' '}
+                    and <span className="text-neon-blue">frontend</span> development. I also have a
+                    knack for <span className="text-neon-blue">design</span> and enjoy building cool
+                    stuff. Thank you for visiting my portfolio!{" "}
                 </p>
 
                 <div>
@@ -50,7 +50,7 @@ function HomePage(props) {
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
