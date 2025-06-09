4287a500d443d4d1403dcc2d9e69795c7327d12a
Eduardo Vera <this.eduardovera@gmail.com>
2025-06-09 18:31:43 -0400

    Creating carousel for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,11 @@
 import ProjectCard from "@/components/cards/ProjectCard";
 import Title from "@/components/titles/Title";
+import { useState } from "react";
+import { IoIosRadioButtonOff, IoIosRadioButtonOn } from "react-icons/io";
 
 function Projects(props) {
     const { projects } = props;
+    const [index, setIndex] = useState(0);
 
     return (
         <section id="what_i've_built" className="section mb-48">
@@ -10,13 +13,30 @@ function Projects(props) {
                 <Title>What I&apos;ve Built</Title>
             </div>
 
-            <ul className="flex justify-center">
-                {projects.map((proj, i) => (
-                    <li key={`project-${i + 1}`}>
-                        <ProjectCard {...proj} orientation={`${i % 2 === 0 ? "right" : "left"}`} />
-                    </li>
-                ))}
+            <ul className={`flex justify-center`}>
+                {projects.map(
+                    (proj, i) =>
+                        index === i && (
+                            <li key={`project-${i + 1}`}>
+                                <ProjectCard
+                                    {...proj}
+                                    orientation={`${i % 2 === 0 ? "right" : "left"}`}
+                                />
+                            </li>
+                        ),
+                )}
             </ul>
+            <div className="flex justify-center">
+                {projects.map((_, i) => (
+                    <i
+                        key={`project-icon-${i + 1}`}
+                        className={`text-neon-blue cursor-pointer text-4xl mt-4`}
+                        onClick={() => setIndex(i)}
+                    >
+                        {i === index ? <IoIosRadioButtonOn /> : <IoIosRadioButtonOff />}
+                    </i>
+                ))}
+            </div>
         </section>
     );
 }
--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,23 +1,13 @@
 function getProjectsData() {
     return [
-        // {
-        //     name: 'Portfolio',
-        //     description:
-        //         'Version 3 of my personal portfolio made it with Next.JS',
-        //     stack: ['Next.js', 'React.js', 'Tailwind.css', 'CSS Modules'],
-        //     img: '/portfolio_1.png',
-        //     link: 'https://eduardovera.vercel.app',
-        //     repo: 'https://github.com/ewa1do/portfolio-next',
-        // },
-        // {
-        //     name: "GYM Management App",
-        //     description:
-        //         "Freelance project: app to manage attendance and payments at my local gym. (Currently on Development)",
-        //     stack: ["Next.js", "React.js", "TypeScript", "Supabase", "Tailwind.css"],
-        //     img: "https://images.unsplash.com/photo-1571902943202-507ec2618e8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
-        //     link: "https://github.com/ewa1do/gym_schedule_app",
-        //     repo: "https://github.com/ewa1do/gym_schedule_app",
-        // },
+        {
+            name: "CryptoFOX Expo",
+            description: "Landing Page created for the blockchain conference CryptoFOX.",
+            stack: ["Next.js", "React.js", "TypeScript", "Tailwind.css"],
+            img: "/portfolio_1.png",
+            link: "https://cryptofoxexpo-demo.vercel.app/",
+            repo: "https://github.com/ewa1do/cryptofox_page",
+        },
         {
             name: "Albano's Landing Page",
             description: "Landing Page for a Pizza Shop made it with pure HTML & CSS",
