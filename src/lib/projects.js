a325522b3cc702059522a2dfc508a2862b35fde0
Edu <eavera97@gmail.com>
2025-06-27 22:23:06 -0400

    refactoring projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,20 +1,29 @@
 function getProjectsData() {
     return [
         {
-            name: "CryptoFOX Expo",
-            description: "Landing Page created for the blockchain conference CryptoFOX.",
-            stack: ["Next.js", "React.js", "TypeScript", "Tailwind.css"],
-            img: "/portfolio_1.png",
-            link: "https://cryptofoxexpo-demo.vercel.app/",
-            repo: "https://github.com/ewa1do/cryptofox_page",
-        },
-        {
             name: "Albano's Landing Page",
             description: "Landing Page for a Pizza Shop made it with pure HTML & CSS",
             stack: ["HTML", "CSS"],
             img: "/portfolio_2.png",
             link: "https://albanos.netlify.app",
             repo: "https://github.com/ewa1do/albanos",
+        },
+        {
+            name: "Sazón Cooking Diary",
+            description:
+                "Web application which allow the user to search, save and create Cooking Recipes",
+            stack: ["HTML", "CSS", "React.js", "Tailwindcss"],
+            img: "/portfolio_3.png",
+            link: "https://sazon-cooking-diary.netlify.app/",
+            repo: "https://github.com/ewa1do/sazon-cooking-diary",
+        },
+        {
+            name: "CryptoFOX Expo",
+            description: "Landing Page created for the blockchain conference CryptoFOX.",
+            stack: ["Next.js", "React.js", "TypeScript", "Tailwind.css"],
+            img: "/portfolio_1.png",
+            link: "https://cryptofoxexpo-demo.vercel.app/",
+            repo: "https://github.com/ewa1do/cryptofox_page",
         },
     ];
 }
